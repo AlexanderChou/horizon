e68f60f45fd6eb33a536ef161e5dbd621d79dddf
Jianping Weng <root@controller.(none)>
2015-04-17 03:53:09 -0400

    first commit

---
+++ 3/openstack_auth/MysqlDBConn.py
@@ -0,0 +1,25 @@
+import MySQLdb
+
+	
+class DBConn:
+	conn = None
+	
+	def connect(self):
+		self.conn= MySQLdb.connect(	
+			host='166.111.143.241',
+			port = 3306,
+			user='root',
+			passwd='tsinghua',
+			db ='test_zxl', 
+			)
+	def cursor(self):
+		try:
+			return self.conn.cursor()
+		except (AttributeError, MySQLdb.OperationalError):
+			self.connect()
+			return self.conn.cursor()
+	def commit(self):
+		return self.conn.commit()	
+	def close(self):
+		return self.conn.close()
+
